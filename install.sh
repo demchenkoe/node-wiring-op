9b5db433674d03d48a55827d82f51a011761ceab
Leandre Gohy <leandre.gohy@hexeo.be>
2014-07-23 20:32:12 +0200

    libWiringPi as a submodule

--- install.sh
+++ install.sh
@@ -39,8 +39,12 @@ check_git_clone() {
 rm ./install.log 2>/dev/null 1>&2
 
 echo -n "Cloning libWiringPi ... "
-rm -Rf ./wiringpi 2>/dev/null 1>&2
-git clone https://github.com/nekuz0r/wiringpi.git > ./install.log 2>&1
+#rm -Rf ./wiringpi 2>/dev/null 1>&2
+#git clone https://github.com/nekuz0r/wiringpi.git > ./install.log 2>&1
+#check_git_clone
+git submodule init
+check_git_clone
+git submodule update
 check_git_clone
 echo "done."
 
